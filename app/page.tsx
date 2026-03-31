29359b84a62ca3fee05ac05dee579a5c8805eab3
kiryuin0121 <162397520+kiryuin0121@users.noreply.github.com>
2026-03-31 14:37:16 +0900

    Sceneの初期設定を行った
    カメラ,照明、controlls,球体を追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,31 @@
+"use client";
+
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
 const App = () => {
   return (
-    <div>App</div>
+    <main className={`w-screen h-screen bg-black text-white`}>
+      <Canvas
+        camera={{position:[0,0,5]}}
+      >
+        <directionalLight
+          color={"#ffffff"}
+          intensity={0.75}
+          position={[5,5,5]}
+        />
+        <ambientLight
+        color={"#ffffff"}
+        intensity={0.2}
+        />
+        <OrbitControls/>
+
+        <mesh>
+          <icosahedronGeometry args={[1,5]}/>
+          <meshStandardMaterial/>
+        </mesh>
+      </Canvas>
+    </main>
   )
 }
 
