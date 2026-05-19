efd090436c35b69b7fce362d13d5238c06114611
kiryuin0121 <162397520+kiryuin0121@users.noreply.github.com>
2026-05-19 17:10:57 +0900

    時間経過に応じて画面の色を変更する

--- shaders/customShaderMaterial/fragment.glsl
+++ shaders/customShaderMaterial/fragment.glsl
@@ -1,7 +1,9 @@
 varying vec3 vPosition; //in
 uniform vec3 uColor; //in
 uniform vec2 uMouse; //in
+uniform float uTime; //in
 void main() {
-  vec3 color = vec3(uMouse.x,0,uMouse.y);
+  // vec3 color = vec3(uMouse.x,0,uMouse.y);
+  vec3 color = vec3(0,abs(sin(uTime)),abs(cos(uTime)));
   gl_FragColor = vec4(color, 1.0);
 }
